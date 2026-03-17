98dc75252868b4387ee3288ef2dba734bdef8c17
Leclercq Tom <t.leclercq@couach.com>
2026-03-17 14:27:07 +0100

    full screen and ajust link

--- app/src/main/java/com/example/magellan/MainActivity.kt
+++ app/src/main/java/com/example/magellan/MainActivity.kt
@@ -88,7 +88,7 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        webView.loadUrl("https://192.168.255.100/app")
+        webView.loadUrl("https://192.168.255.100/performance")
     }
 
     // 🔒 bloque bouton retour (mode app drone)
